321f377efc5dca398020b16f79cd2ad07b1dee86
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-04 23:29:59 +0900

    Bird class 초기화 후 update, draw 코드 추가

--- bird.py
+++ bird.py
@@ -3,18 +3,27 @@ import game_framework
 
 class Bird:
     def __init__(self):
-        pass
+        self.image = load_image('bird_animation.png')
+        self.x, self.y = 400, 400
+        self.size = 100
+        self.dir = 1
+        self.speed = 10#일단 상수
+        self.frame = 0
+
+        self.fly = Fly(self)
+        self.state = self.fly
+        self.state.enter()  # 상태 진입
 
     def update(self):
-        pass
+        self.state.do()
 
     def draw(self):
-        pass
+        self.state.draw()
 
 
 class Fly:
-    def __init__(self):
-        pass
+    def __init__(self, bird):
+
 
     def enter(self):
         pass
