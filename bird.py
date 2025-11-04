aca45f0aad8686d05e73f6841a4944e2f4c714a2
Cho-Jeonghyeon <titanfall120@naver.com>
2025-11-04 23:23:31 +0900

    bird.py 생성, Bird, Fly class 생성 후 기본 구조 작성

---
+++ bird.py
@@ -0,0 +1,30 @@
+from pico2d import *
+import game_framework
+
+class Bird:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+class Fly:
+    def __init__(self):
+        pass
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        pass
+
